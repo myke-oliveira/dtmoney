58dfbed88b59eda1d2aee3e754deddc2354a703f
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2022-03-27 22:25:41 -0300

    🚧 PROGRESS: create new transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import Modal from "react-modal";
+
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
