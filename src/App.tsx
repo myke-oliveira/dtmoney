ceb9b7c23efc8426d028fc5ab419e60ac309bd42
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2022-03-27 22:34:27 -0300

    🚧 PROGRESS: create new transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
 
+Modal.setAppElement('#root');
+
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
